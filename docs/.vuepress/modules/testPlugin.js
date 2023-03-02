a19dc4c4d9e4f2b741e69a950ba14ac25ce865ec
moody4 <moodyallen7@gmail.com>
2023-03-03 08:35:50 +1000

    fix: build error caused by incorrect order of parsing pages

--- docs/.vuepress/modules/testPlugin.js
+++ docs/.vuepress/modules/testPlugin.js
@@ -1,4 +1,7 @@
 import { fs as _fs, path as _path } from '@vuepress/utils'
+import { fileURLToPath } from 'node:url'
+
+const meta = await _fs.readJSON(fileURLToPath(new URL(`../modules/authorsMeta.json`, import.meta.url)))
 
 export const testPlugin = (/* options */) => (app) => {
    
@@ -8,6 +11,46 @@ export const testPlugin = (/* options */) => (app) => {
    const _softwarePages = {}
    const _scriptsPages = []
    const _tags = []
+
+   const authorsInBase = Object.entries(meta).length
+
+   const objRedinessWaiter = async (object, options = {}) => {
+
+      const { 
+         maxInterval = 500,
+         delay = 100,
+         errorMsg = `Waiting period has exceeded the allowable limit`,
+         targetObjLength = 0 
+      } = options
+
+      const agnostic = targetObjLength === 0
+      let waited = 0
+      let lastLength = 0
+
+      return new Promise ((resolve, reject) => {
+         setTimeout(function tick () {
+            const length = Object.entries(object).length
+            waited += (lastLength === length)? delay : -waited
+            if (waited >= maxInterval) {
+               if (agnostic || length === targetObjLength) resolve(true)
+               reject(new Error(errorMsg))
+            } else {
+               lastLength = length
+               setTimeout(tick, delay);
+            }
+         })
+      }).catch((error) => { throw error })
+   }
+
+   const scriptsPagesWaiter = objRedinessWaiter(_scriptsPages)
+   const softwarePagesWaiter = objRedinessWaiter(_softwarePages)
+   const authorsPagesWaiter = objRedinessWaiter(_authorsPages, 
+   { 
+      targetObjLength: authorsInBase, 
+      errorMsg: `Failed to prepare authors data before compiling. It's either lack of author info in json file or missing author page.`
+   })
+
+   const escapeStr = (string) => string.toLowerCase().replace(/\s/g, '-')
 
    const pathStartsWith = (path, URISegment) => path?.startsWith(app.dir.source(URISegment))
 
@@ -72,7 +115,8 @@ export const testPlugin = (/* options */) => (app) => {
 
       if(pathStartsWith(filePath, 'authors/') && !filePath.endsWith('index.md')){
          page.frontmatter.sidebar = parsePageHeaders(page).map(convertHeaderToSidebarItem)
-         _authorsPages[page.title] = page
+         const slug = escapeStr(page.title)
+         _authorsPages[slug] = page
       } 
       
       else if(pathStartsWith(filePath, 'software/')){
@@ -82,61 +126,43 @@ export const testPlugin = (/* options */) => (app) => {
       
       else if (pathStartsWith(filePath, 'scripts/')) {
 
-         page.frontmatter.sidebar = false
-         // Embedding <ScriptPageHeader> component in a script page
-         page.sfcBlocks.template.contentStripped = page.sfcBlocks.template.contentStripped.replace(/^<h2.+<\/h2>\n/, (m) => `${m}<ScriptPageHeader pageKey="${page.key}" />`)
+         const addToPageSidebar = (page, scriptPage) => {
+            const child = {text: scriptPage.title, link: `#${scriptPage.title.toLowerCase()}`, children: []}
+            page.frontmatter.sidebar[1].children.push(child)
+            page.frontmatter.sidebar[1].children.sort((a, b) => a.text === b.text ? 0 : (a.text < b.text ? -1 : 1))
+         }
          
          _scriptsPages.push(page)
-         _tags.push(...page.frontmatter.tags);
-
-         const addToPageSidebar = async (page, scriptPage) => {
-            await new Promise((resolve, reject) => {
-               let waitedFor = 0
-               let delay = 100
-               setTimeout(function tick () {
-                  if (page === undefined) {
-                     waitedFor += delay
-                     if (waitedFor >= 1000) reject(new Error(`Failed to add item to a page sidebar, page probably doesn't exist.\n Problem with frontmatter of the script page: ${scriptPage.path}`))
-                     setTimeout(tick, delay) 
-                  }
-                  else {
-                     const child = {text: scriptPage.title, link: `#${scriptPage.title.toLowerCase()}`, children: []}
-                     page.frontmatter.sidebar[1].children.push(child)
-                     page.frontmatter.sidebar[1].children.sort((a, b) => a.text === b.text ? 0 : (a.text < b.text ? -1 : 1))
-                     resolve()
-                  }
-               }, delay)
-            })
-            .catch((error) => { throw error } )
-         }
-
-         const author = page.frontmatter.author
+         _tags.push(...page.frontmatter.tags)
+         
+         page.frontmatter.sidebar = false
+         page.sfcBlocks.template.contentStripped = page.sfcBlocks.template.contentStripped.replace(/^<h2.+<\/h2>\n/, (m) => `${m}<ScriptPageHeader pageKey="${page.key}" />`)
+         
+         const slug = escapeStr(page.frontmatter.author)
          const software = page.frontmatter.software
+         
+         await authorsPagesWaiter
+         addToPageSidebar(_authorsPages[slug], page)
 
-         await addToPageSidebar(_authorsPages[author], page)
-         await addToPageSidebar(_softwarePages[software], page)
+         await softwarePagesWaiter
+         addToPageSidebar(_softwarePages[software], page)
          
       }
 
       else if (pathStartsWith(filePath, 'tags/')) {
 
-         const fillTagsPageSidebar = async () => {
-            await new Promise((resolve, reject) => {
-               // Is there a way to know exactly whether all tags have been collected, other than setting an arbitrary counter?
-               setTimeout(() => resolve(_tags), 1100)
-            })
-            .then((result) => {
-               const tags = [...(new Set(result))].sort()
-               const children = tags.map(tag => { return {text: tag, link: `#${tag.toLowerCase()}`} })
-               page.frontmatter.sidebar = [{text: 'Tags', children: children}]
-
-               // Clear arrays in dev server scope for HMR
-               _scriptsPages.length = 0;
-               _tags.length = 0;
-            })
+         const fillTagsPageSidebar = () => {
+            const tags = [...(new Set(_tags))].sort()
+            const children = tags.map(tag => { return {text: tag, link: `#${tag.toLowerCase()}`} })
+            page.frontmatter.sidebar = [{text: 'Tags', children: children}]
          }
+         
+         await scriptsPagesWaiter
+         fillTagsPageSidebar()
 
-         await fillTagsPageSidebar()
+         // Clear arrays in dev server scope for HMR
+         _tags.length = 0
+         _scriptsPages.length = 0
          
       }
 
